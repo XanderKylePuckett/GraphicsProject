ecec89dc6ababfd266f3a9f51d120100de73efb2
Antonio Perez <tonyboyangie3@gmail.com>
2016-12-03 17:47:28 -0500

    skgry

--- Solution/DX11UWA/Content/PixelShaderSky.hlsl
+++ Solution/DX11UWA/Content/PixelShaderSky.hlsl
@@ -1,6 +1,18 @@
 textureCUBE tex : register( t0 );
 SamplerState samplerstate : register( s0 );
 
+cbuffer LightingConstantBuffer : register( b0 )
+{
+	float4 lightState;
+	float4 dLightDirection;
+	float4 pLightPos0;
+	float4 pLightPos1;
+	float4 pLightPos2;
+	float4 pLightColorRadius0;
+	float4 pLightColorRadius1;
+	float4 pLightColorRadius2;
+};
+
 struct PixelShaderInput
 {
 	float4 pos : SV_POSITION;
@@ -12,5 +24,14 @@ struct PixelShaderInput
 
 float4 main( PixelShaderInput input ) : SV_TARGET
 {
-	return tex.Sample( samplerstate, input.posL.xyz );
+	float4 result = tex.Sample( samplerstate, input.posL.xyz );
+	if ( lightState.w == 1.0f ) result = float4( result.x, result.x * 0.95f, result.x * 0.95f, 1.0f );
+	else if ( lightState.w == 2.0f ) result = float4( result.y * 0.95f, result.y, result.y * 0.95f, 1.0f );
+	else if ( lightState.w == 3.0f ) result = float4( result.z * 0.95f, result.z * 0.95f, result.z, 1.0f );
+	else if ( lightState.w == 4.0f )
+	{
+		float color = ( result.x + result.y + result.z ) / 3.0f;
+		result = float4( color, color, color, 1.0f );
+	}
+	return result;
 }
